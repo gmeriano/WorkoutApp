bec5b4a4c4ab14bd212f6e0f1ed7aac826d8ec20
gmeriano <gvmeriano@gmail.com>
2025-05-22 00:29:03 -0500

    Setup skeleton tab structure

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from 'expo-router';
+import Foundation from '@expo/vector-icons/Foundation';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+export default function TabsLayout() {
+    return <Tabs screenOptions={{ tabBarActiveTintColor: 'black' }} >
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: 'Workout Log',
+                headerShown: false,
+                tabBarIcon: ({ size, color }) => <Foundation name='clipboard-notes' size={size} color={color} />,
+            }}/>
+        <Tabs.Screen
+            name="workouts"
+            options={{
+                title: 'Workouts',
+                headerShown: false,
+                tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name='weight-lifter' size={size} color={color} />,
+            }}/>
+        <Tabs.Screen
+            name="exercises"
+            options={{
+                title: 'Exercises',
+                headerShown: false,
+                tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name='dumbbell' size={size} color={color} />,
+            }}/>
+        </Tabs>
+}
---
+++ src/app/(tabs)/exercises.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+export default function Exercises() {
+    return (
+        <Text>Exercises</Text>
+    )
+}
