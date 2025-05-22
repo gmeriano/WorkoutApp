ce95fab9d4483d57c7996c66790a253c596f53dd
gmeriano <gvmeriano@gmail.com>
2025-05-22 00:47:39 -0500

    path alias setup

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function WorkoutLog() {
   return (
     <View>
-      <Text className='text-3xl font-bold text-red-500'>Workout Log</Text>
+      <Text className='text-3xl font-bold text-red-500 p-2'>Workout Log</Text>
     </View>
   );
 }
